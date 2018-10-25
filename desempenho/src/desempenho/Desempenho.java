50c1f0ac58fb15b0b1610bf3b01ec9ca3a667b24
Gustavo <Gustavo@DESKTOP-VBQ8UMH>
2018-10-25 20:20:24 -0300

    classes e pacotes

--- desempenho/src/desempenho/Desempenho.java
+++ desempenho/src/desempenho/Desempenho.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package desempenho;
+public class Orcamento {
+private double valor;
+public Orcamento(double valor) {
+this.valor = valor;
+}
+public double getValor() {
+return valor;
+}
+}
 
-/**
- *
- * @author Gustavo
- */
-public class Desempenho {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public class Imposto {
+public Imposto() {
+}
+public double calcula(Orcamento orcamento) {
+System.out.println("teste");
+return orcamento.getValor() * 0.1;
+}
 }
